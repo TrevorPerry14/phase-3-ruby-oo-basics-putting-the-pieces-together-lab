c4d28e6a99b980b13b4b16a56be57514eac868ac
Trevor <trevorperry143@gmail.com>
2022-11-01 19:55:53 -0500

    commit tree 288d922fd11c4d7ddf38f912068143f5b94386be

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+the_hobbit = Book.new("The Hobbit")
+the_hobbit.title
+the_hobbit.author = "Tolkien"
+the_hobbit.page_count = 368
+the_hobbit.genre = "Fantasy"
+the_hobbit.turn_page
